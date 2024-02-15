bfc93eb92d91b7c9ce0fe96d6e6393ef2dac7ac7
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-16 04:20:02 +0630

    update the UserList

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function UserList() {
-  return <div>UserList</div>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:8080/crud_users");
+    setUsers(response.data);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <button className="btn btn-success">Add +</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <button className="btn btn-primary">Edit</button>
+                  <button className="btn btn-danger ms-2">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default UserList;
