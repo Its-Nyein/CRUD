0c54f57a7d9489ac2f03e4c8c0f40228589458e4
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-16 01:51:17 +0630

    update the controller

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getUsers } from "../controller/UserController.js";
+import { 
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser 
+} from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get('/crud_users', getUsers);
+router.get('/crud_users/:id', getUserById);
+router.post('/crud_users', createUser);
+router.patch('/crud_users/:id', updateUser);
+// router.delete('/crud_users/:id', deleteUser);
 
-export default getUsers;
+export default router;
