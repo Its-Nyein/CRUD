3a9db2d5fd80aa20fefbffca00afa98804057b10
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-15 22:06:50 +0630

    use ES6 syntax to import and export modules

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import UserRoutes from "./routes/UserRoutes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use(UserRoutes())
 
 app.listen(8080, () => console.log("server running"))
